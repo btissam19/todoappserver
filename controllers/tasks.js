e1804b5e9ee8bcffa1be9c337947546be030736b
Ibtissam <btissam1234btissam12@gmail.com>
2023-12-11 13:13:21 +0100

    connect things to frontend with id problem and also the .env file absceans

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,50 +2,56 @@
 import { db } from '../database/connection.js';
 
 export const addingTask = async (req, res) => {
-  const { text, completed } = req.body;
-  if (!text || typeof completed !== 'boolean') {
+  const { text } = req.body;
+
+  if (!text) {
     return res.status(400).send('Invalid request data');
   }
 
-  const sql = 'INSERT INTO tasks (text, completed) VALUES (?, ?)';
+  const sql = 'INSERT INTO tasks (text) VALUES (?)';
 
   try {
-    const result = await db.query(sql, [text, completed]);
+    const result = await db.query(sql, [text]);
     console.log('Task added successfully');
-    res.json({ text, completed });
+    res.json({ text, completed: false }); // You can still send completed: false here if you want
   } catch (err) {
     console.error('Error adding task:', err);
     res.status(500).send('Internal Server Error');
   }
 };
 
+
 export const getAllTasks = async (req, res) => {
   const sql = 'SELECT * FROM tasks';
     db.query(sql, function (err, result) {
       if (err) throw err;
       else {
-       res.json(result);
+       
+      
+  
+        res.json(result);
       }
     })
   }
-export const deleteTask = async (req, res) => {
-  const taskId = req.query.id;
-  const sql = 'DELETE FROM tasks WHERE id = ?';
-
-  try {
-    const result = await db.query(sql, [taskId]);
-
-    if (result.affectedRows === 0) {
-      res.status(404).json({ error: 'Task not found' });
-    } else {
-      console.log('Task deleted successfully');
-      res.json({ message: 'Task deleted successfully' });
+  export const deleteTask = async (req, res) => {
+    const taskId = req.params.id; // Use params to get the task ID from the URL
+    const sql = 'DELETE FROM tasks WHERE id = ?';
+  
+    try {
+      const result = await db.query(sql, [taskId]);
+  
+      if (result.affectedRows === 0) {
+        res.status(404).json({ error: 'Task not found' });
+      } else {
+        console.log('Task deleted successfully');
+        res.json({ message: 'Task deleted successfully' });
+      }
+    } catch (err) {
+      console.error('Error deleting task:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-  } catch (err) {
-    console.error('Error deleting task:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+  };
+  
 
 export const updateTask=async (req, res) => {
   const id = req.params.id;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import bodyParser from "body-parser"; // Import body-parser
+import bodyParser from "body-parser";
+import cors from 'cors'; // Correct import statement
 
 import { db } from "./database/connection.js";
 import { createTasksTable } from "./models/tasksTable.js";
@@ -8,8 +9,8 @@ import { router } from "./routes/task.js";
 const port = 4000;
 const app = express();
 
-app.use(bodyParser.json()); // Use body-parser middleware for JSON parsing
-
+app.use(cors());
+app.use(bodyParser.json());
 app.use('/', router);
 
 db.connect((err) => {
@@ -23,3 +24,76 @@ db.connect((err) => {
     });
   }
 });
+
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const cors = require('cors');
+// const { json } = require('body-parser');
+// const { nanoid } = require('nanoid');
+
+// dotenv.config({ path: './config.env' });
+
+// const app = express();
+
+// app.use(cors());
+// app.use(json());
+
+// let todos = [
+// 	{
+// 		id: nanoid(),
+// 	text: 'todo 1',
+// 		completed: true,
+// 	},
+// 	{
+// 		id: nanoid(),
+// 	text: 'todo 2',
+// 		completed: false,
+// 	},
+// 	{
+// 		id: nanoid(),
+// 	text: 'todo 3',
+// 		completed: false,
+// 	},
+// 	{
+// 		id: nanoid(),
+// 	text: 'todo 4',
+// 		completed: false,
+// 	},
+// 	{
+// 		id: nanoid(),
+// 	text: 'todo 5',
+// 		completed: false,
+// 	},
+// ];
+
+// app.get('/todos', (req, res) => res.send(todos));
+
+// app.post('/todos', (req, res) => {
+// 	const todo = { text: req.body.text, id: nanoid(), completed: false };
+// 	todos.push(todo);
+// 	return res.send(todo);
+// });
+
+// app.patch('/todos/:id', (req, res) => {
+// 	const id = req.params.id;
+// 	const index = todos.findIndex((todo) => todo.id == id);
+// 	const completed = Boolean(req.body.completed);
+// 	if (index > -1) {
+// 		todos[index].completed = completed;
+// 	}
+// 	return res.send(todos[index]);
+// });
+
+// app.delete('/todos/:id', (req, res) => {
+// 	const id = req.params.id;
+// 	const index = todos.findIndex((todo) => todo.id == id);
+// 	if (index > -1) {
+// 		todos.splice(index, 1);
+// 	}
+
+// 	res.send(todos);
+// });
+
+// const PORT = 7000;
+
+// app.listen(PORT, console.log(`Server running on port ${PORT}`));
